12fe19d2b702d321d64e5f7ae25ad0f18e398cbb
Bryan Oyloe <boyloe@gmail.com>
2020-06-19 19:11:59 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -20,7 +20,7 @@ def update_status
       }
    }
    epic_tragedy[montague][hero][status] = "dead"
-   epic_tragedy[capulet][heroine][sta] = "dead"  
+   epic_tragedy[capulet][heroine][status] = "dead"  
   
 
 
